efb774247a6d2cd951629960660998b9ffa1599b
uolton <valter.bricca@gmail.com>
2014-11-24 12:39:21 +0100

    initial baseline

---
+++ DesignByContractsResources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using MFramework.Common.Core.Resource;
+using MFramework.Common.Core.Resource.Containers;
+
+using MFramework.Common.Core.Resource.ResourceTypes;
+
+namespace MFramework.Common.DesignByContracts
+{
+    internal class DesignByContractsResources
+    {
+        private DesignByContractsResources() {}
+        public static IResource<string> ArgumentNullExceptionMessage =
+            ResourceManager<Resx<Resx.ResxConfigurationOnAssemblyDefault<DesignByContractsResources>>>.Register(new ResourceString("ArgumentNullException"));
+    }
+}
